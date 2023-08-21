698a79b67d716f6dbcfefb3e50919262891a449e
Comfy <kenwadder27@gmail.com>
2023-08-21 13:21:52 -0500

    autoplay works

--- pages/components/Videoplayer.js
+++ pages/components/Videoplayer.js
@@ -7,15 +7,18 @@ import ReactPlayer from 'react-player';
 
 
 function VideoPlayer() {
-    const videoUrl = 'https://www.youtube.com/embed/piNI6hRyPzM?wmode=transparent&autoplay=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&list=PLFaEw-2w6HsVZs8hmWdbpEaaRCuZ1V35a'
+    const videoUrl = 'https://www.youtube.com/embed/piNI6hRyPzM?autoplay=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&list=PLFaEw-2w6HsVZs8hmWdbpEaaRCuZ1V35a'
     return (
         <>
             
-            <iframe src={videoUrl} 
-            title="YouTube video player" wmode="Opaque" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; opaque" >
+            <iframe src={videoUrl}
+            title="Hero"frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen allowTransparency >
             </iframe>
         </>
     )
 }
 export default VideoPlayer;
 
+
+
+<iframe width="1234" height="694" src="https://www.youtube.com/embed/piNI6hRyPzM?wmode=transparent&autoplay=1" title="HERO" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
