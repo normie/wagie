e9d48bcbf321eb2955aaedc41da091891fe1fa6a
Comfy <kenwadder27@gmail.com>
2023-08-21 00:23:53 -0500

    next.js deployment

--- pages/components/Videoplayer.js
+++ pages/components/Videoplayer.js
@@ -5,14 +5,14 @@ import { keyframes } from "@mui/material";
 import ReactPlayer from 'react-player';
 
 
+
 function VideoPlayer() {
     const videoUrl = 'https://www.youtube.com/embed/piNI6hRyPzM?wmode=transparent&autoplay=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&list=PLFaEw-2w6HsVZs8hmWdbpEaaRCuZ1V35a'
     return (
         <>
             
             <iframe src={videoUrl} 
-            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowfullscreen >
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen >
             </iframe>
         </>
     )
