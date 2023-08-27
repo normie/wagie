c0a53988000ab5bde825fd7f80c6a9966c1bbf86
Comfy <kenwadder27@gmail.com>
2023-08-27 15:31:51 -0500

    retrieving videoplayer state

--- pages/components/Videoplayer.js
+++ pages/components/Videoplayer.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { YoutubePlayer} from "youtube-player";
 import Button from '@mui/material/Button';
 import { Box,Container, Paper, Slide } from "@mui/material";
 import { keyframes } from "@mui/material";
@@ -7,12 +8,34 @@ import ReactPlayer from 'react-player';
 
 
 function VideoPlayer() {
+    const iframeRef = useRef(null);
+
+    useEffect(() => {
+        const player = new YoutubePlayer(iframeRef.current, {
+            videoId: "piNI6hRyPzM",
+        });
+        player.loadVideo();
+
+
+        player.on('playerStateChange', (event) => {
+            console.log(event.data.toString());
+        });
+       
+    },[iframeRef]);
+
+
+
     const videoUrl = 'https://www.youtube.com/embed/piNI6hRyPzM?autoplay=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&list=PLFaEw-2w6HsVZs8hmWdbpEaaRCuZ1V35a'
+    
     return (
         <>
             
-            <iframe src={videoUrl}
-            title="Hero"frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen allowTransparency >
+            <iframe 
+            ref={iframeRef}
+            title="Hero"frameborder="0" 
+            
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen allowTransparency >
             </iframe>
         </>
     )
@@ -21,4 +44,3 @@ export default VideoPlayer;
 
 
 
-<iframe width="1234" height="694" src="https://www.youtube.com/embed/piNI6hRyPzM?wmode=transparent&autoplay=1" title="HERO" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
